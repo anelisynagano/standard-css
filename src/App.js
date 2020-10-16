239c6a74c49f5f4f9aa250560e97c8641d265c47
WildCodeSchool <anelisynagano@gmail.com>
2020-10-16 18:37:58 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+// import the CSS file here to see it working!
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>Standard CSS!</h1>
+      <p>This is an example of standard CSS</p>
     </div>
   );
 }
